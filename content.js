edd86c8b830cc60e05cb2d6df4ced91df61393a4
kiefferbp <kiefferbp@gmail.com>
2016-07-03 14:34:59 -0500

    various: emulate userAgent, have default values

--- content.js
+++ content.js
@@ -3,8 +3,14 @@ chrome.storage.sync.get(null, function (storageContents) {
     script.text = "";
 
     Object.keys(storageContents).forEach(function (key) {
-        script.text += "navigator.__defineGetter__('" + key + "', () => '" + storageContents[key] + "');";
-        script.text += "console.log('Faked the value of navigator." + key + " to " + storageContents[key] + "');";
+        var fakedValue = storageContents[key];
+
+        if (!fakedValue) {
+            fakedValue = navigator[key];
+        }
+
+        script.text += "navigator.__defineGetter__('" + key + "', () => '" + fakedValue + "');";
+        script.text += "console.log('Faked the value of navigator." + key + " to " + fakedValue + "');";
     });
 
     (document.head || document.documentElement).appendChild(script);
